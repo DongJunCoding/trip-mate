0d782a7f3abf7143c89bb6e0cb25b9987f4caffc
dongjun <rkawksha@naver.com>
2025-12-26 23:00:55 +0900

    코드정리

--- backend/src/main/java/com/server/backend/common/auth/jwt/filter/JWTFilter.java
+++ backend/src/main/java/com/server/backend/common/auth/jwt/filter/JWTFilter.java
@@ -17,7 +17,6 @@ public class JWTFilter extends OncePerRequestFilter {
 
     private final JWTUtil jwtUtil;
 
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 

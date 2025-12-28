faf76b4f8f6e0cd59250f8b8904b0d1572f24aba
dongjun <rkawksha@naver.com>
2025-12-28 23:55:09 +0900

    JWTFilter 구현

--- backend/src/main/java/com/server/backend/common/auth/jwt/filter/JWTFilter.java
+++ backend/src/main/java/com/server/backend/common/auth/jwt/filter/JWTFilter.java
@@ -1,12 +1,16 @@
 package com.server.backend.common.auth.jwt.filter;
 
 import com.server.backend.common.auth.jwt.util.JWTUtil;
+import com.server.backend.common.auth.security.auth.CustomUserDetails;
+import com.server.backend.common.data.entity.UserEntity;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -14,14 +18,53 @@ import java.io.IOException;
 @Slf4j
 @RequiredArgsConstructor
 public class JWTFilter extends OncePerRequestFilter {
-    // 스프링 시큐리티 filter chain에 요청에 담긴 JWT를 검증하기 위한 커스텀 필터
-    // 해당 필터를 통해 요청 헤더 Authorization 키에 JWT가 존재하는 경우 JWT를 검증하고 강제로 SecurityContextHolder에 세션을 생성
-    // (이 세션은 STATELESS 상태로 관리되기 때문에 해당 요청이 끝나면 소멸)
+    /**
+     * 스프링 시큐리티 filter chain에 요청에 담긴 JWT를 검증하기 위한 커스텀 필터
+     * 해당 필터를 통해 요청 헤더 Authorization 키에 JWT가 존재하는 경우 JWT를 검증하고 강제로 SecurityContextHolder에 세션을 생성
+     * (이 세션은 STATELESS 상태로 관리되기 때문에 해당 요청이 끝나면 소멸)
+     */
 
     private final JWTUtil jwtUtil;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        String authorization = request.getHeader("Authorization");
+
+        // Authorization 헤더 검증
+        if(authorization == null || !authorization.startsWith("Bearer")) {
+            log.info("token null");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authorization.split(" ")[1];
+
+        // 토큰 소멸시간 검증
+        if(jwtUtil.isExpired(token)) {
+            log.info("token expired");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // token에서 username, role 획득
+        String username = jwtUtil.getUsername(token);
+        String role = jwtUtil.getRole(token);
+
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserId(username);
+        userEntity.setUserPw("temppassword");
+        userEntity.setUserRole(role);
+
+        // UserDetails에 회원 정보 객체 담기
+        CustomUserDetails customUserDetails = new CustomUserDetails(userEntity);
+
+        // 스프링 시큐리티 인증 토큰 생성
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
+
+        // 세션에 사용자 등록
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        filterChain.doFilter(request, response);
     }
 }

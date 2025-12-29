a7237ddc02ef9fabecc03dba3f8d158433934b4b
dongjun <rkawksha@naver.com>
2025-12-29 16:58:09 +0900

    JWT Refresh / Access Token 발급

--- backend/src/main/java/com/server/backend/common/auth/jwt/filter/LoginFilter.java
+++ backend/src/main/java/com/server/backend/common/auth/jwt/filter/LoginFilter.java
@@ -6,10 +6,12 @@ import com.server.backend.common.auth.jwt.util.JWTUtil;
 import com.server.backend.common.auth.security.auth.CustomUserDetails;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -48,28 +50,40 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
         log.info("## successfulAuthentication");
 
-        // 유저 객체를 알아내기 위함
-        CustomUserDetails customUserDetails = (CustomUserDetails) authResult.getPrincipal();
+        // 유저 정보
+        String username = authResult.getName();
 
-        String username = customUserDetails.getUsername();
-
+        // 유저 권한
         Collection<? extends GrantedAuthority> authorities = authResult.getAuthorities();
         Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
         GrantedAuthority auth = iterator.next();
-
         String role = auth.getAuthority();
 
-        //TODO 만료시간 yaml로 관리
-        String token = jwtUtil.createJwt("", username, role, 60*60*10L);
+        // 토큰 생성
+        String accessToken = jwtUtil.createJwt("access", username, role, 600000L);
+        String refreshToken = jwtUtil.createJwt("refresh", username, role, 86400000L);
 
-        // HTTP 인증 방식은 RFC 7235 정의에 따라 아래와 같이 인증 헤더 형태를 가져야 한다.
-        response.addHeader("Authorization", "Bearer " + token);
+        // 응답설정, HTTP 인증 방식은 RFC 7235 정의에 따라 아래와 같이 인증 헤더 형태를 가져야 한다.
+        response.setHeader("access", accessToken);
+        response.addCookie(createCookie("refresh", refreshToken));
+        response.setStatus(HttpStatus.OK.value());
     }
 
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
         log.info("unsuccessfulAuthentication");
         response.setStatus(401);
+    }
+
+    private Cookie createCookie(String key, String value) {
+
+        Cookie cookie = new Cookie(key, value);
+        cookie.setMaxAge(24*60*60);
+        // cookie.setSecure(true); https 통신을 진행할 경우 넣어주기
+        // cookie.setPath("/"); 쿠키가 적용될 범위
+        cookie.setHttpOnly(true); // 클라이언트단에서 자바스크립트로 해당 쿠키를 접근하지 못하도록 막는 설정
+
+        return cookie;
     }
 
 }

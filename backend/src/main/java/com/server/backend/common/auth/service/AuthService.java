f5e2969096cf634731fa54ea3b5c275d5b462e63
dongjun <rkawksha@naver.com>
2026-01-04 23:18:16 +0900

    주석

--- backend/src/main/java/com/server/backend/common/auth/service/AuthService.java
+++ backend/src/main/java/com/server/backend/common/auth/service/AuthService.java
@@ -91,86 +91,86 @@ public class AuthService {
     }
 
 
-    public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response) {
-        log.info("## AuthService reissue");
-
-        // get refresh token
-        String refresh = null;
-        Cookie[] cookies = request.getCookies();
-        for(Cookie cookie : cookies) {
-            if(cookie.getName().equals("refresh")) {
-                refresh = cookie.getValue();
-            }
-        }
-
-        if(refresh == null) {
-            // response status code
-            return new ResponseEntity<>("refresh token null", HttpStatus.BAD_REQUEST);
-
-        }
-
-        // expired check
-        try{
-            jwtUtil.isExpired(refresh);
-        } catch (ExpiredJwtException e) {
-            return new ResponseEntity<>("refresh token expired", HttpStatus.BAD_REQUEST);
-        }
-
-        // 토큰이 refresh인지 확인(발급시 페이로드에 명시)
-        String category = jwtUtil.getCategory(refresh);
-
-        if(!category.equals("refresh")) {
-            // response status code
-            return new ResponseEntity<>("invalid refresh token", HttpStatus.BAD_REQUEST);
-        }
-
-        // DB에 저장되어 있는지 확인
-        Boolean isExist = userTokenRepository.existsByRefreshToken(refresh);
-        if(!isExist) {
-            // response body
-            return new ResponseEntity<>("invalid refresh token", HttpStatus.BAD_REQUEST);
-        }
-
-        String username = jwtUtil.getUsername(refresh);
-        String role = jwtUtil.getRole(refresh);
-
-        // make new JWT
-        String newAccess = jwtUtil.createJwt("access", username, role, 6000000L);
-        String newRefresh = jwtUtil.createJwt("refresh", username, role, 86400000L);
-
-        // refresh 토큰 저장 DB에 기존의 Refresh 토큰 삭제 후 새 Refresh 토큰 저장
-        userTokenRepository.deleteByRefreshToken(refresh);
-        addRefreshEntity(username, newRefresh, 86400000L);
-
-        // response
-        response.setHeader("Authorization","Bearer " + newAccess);
-        response.addCookie(createCookie("refresh", newRefresh));
-
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    private Cookie createCookie(String key, String value) {
-        log.info("## AuthService createCookie");
-
-        Cookie cookie = new Cookie(key, value);
-        cookie.setMaxAge(24*60*60);
-        // cookie.setSecure(true);
-        // cookie.setPath("/");
-        cookie.setHttpOnly(true);
-
-        return cookie;
-    }
-
-    private void addRefreshEntity(String username, String refresh, Long expiredMs) {
-        log.info("## AuthService addRefreshEntity");
-
-        Date date = new Date(System.currentTimeMillis() + expiredMs);
-
-        UserTokenEntity userTokenEntity = new UserTokenEntity();
-        userTokenEntity.setUserId(username);
-        userTokenEntity.setRefreshToken(refresh);
-        userTokenEntity.setExpiration(date.toString());
-
-        userTokenRepository.save(userTokenEntity);
-    }
+//    public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response) {
+//        log.info("## AuthService reissue");
+//
+//        // get refresh token
+//        String refresh = null;
+//        Cookie[] cookies = request.getCookies();
+//        for(Cookie cookie : cookies) {
+//            if(cookie.getName().equals("refresh")) {
+//                refresh = cookie.getValue();
+//            }
+//        }
+//
+//        if(refresh == null) {
+//            // response status code
+//            return new ResponseEntity<>("refresh token null", HttpStatus.BAD_REQUEST);
+//
+//        }
+//
+//        // expired check
+//        try{
+//            jwtUtil.isExpired(refresh);
+//        } catch (ExpiredJwtException e) {
+//            return new ResponseEntity<>("refresh token expired", HttpStatus.BAD_REQUEST);
+//        }
+//
+//        // 토큰이 refresh인지 확인(발급시 페이로드에 명시)
+//        String category = jwtUtil.getCategory(refresh);
+//
+//        if(!category.equals("refresh")) {
+//            // response status code
+//            return new ResponseEntity<>("invalid refresh token", HttpStatus.BAD_REQUEST);
+//        }
+//
+//        // DB에 저장되어 있는지 확인
+//        Boolean isExist = userTokenRepository.existsByRefreshToken(refresh);
+//        if(!isExist) {
+//            // response body
+//            return new ResponseEntity<>("invalid refresh token", HttpStatus.BAD_REQUEST);
+//        }
+//
+//        String username = jwtUtil.getUsername(refresh);
+//        String role = jwtUtil.getRole(refresh);
+//
+//        // make new JWT
+//        String newAccess = jwtUtil.createJwt("access", username, role, 6000000L);
+//        String newRefresh = jwtUtil.createJwt("refresh", username, role, 86400000L);
+//
+//        // refresh 토큰 저장 DB에 기존의 Refresh 토큰 삭제 후 새 Refresh 토큰 저장
+//        userTokenRepository.deleteByRefreshToken(refresh);
+//        addRefreshEntity(username, newRefresh, 86400000L);
+//
+//        // response
+//        response.setHeader("Authorization","Bearer " + newAccess);
+//        response.addCookie(createCookie("refresh", newRefresh));
+//
+//        return new ResponseEntity<>(HttpStatus.OK);
+//    }
+//
+//    private Cookie createCookie(String key, String value) {
+//        log.info("## AuthService createCookie");
+//
+//        Cookie cookie = new Cookie(key, value);
+//        cookie.setMaxAge(24*60*60);
+//        // cookie.setSecure(true);
+//        // cookie.setPath("/");
+//        cookie.setHttpOnly(true);
+//
+//        return cookie;
+//    }
+//
+//    private void addRefreshEntity(String username, String refresh, Long expiredMs) {
+//        log.info("## AuthService addRefreshEntity");
+//
+//        Date date = new Date(System.currentTimeMillis() + expiredMs);
+//
+//        UserTokenEntity userTokenEntity = new UserTokenEntity();
+//        userTokenEntity.setUserId(username);
+//        userTokenEntity.setRefreshToken(refresh);
+//        userTokenEntity.setExpiration(date.toString());
+//
+//        userTokenRepository.save(userTokenEntity);
+//    }
 }

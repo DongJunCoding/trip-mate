7f849cc892c9e3e710f548c8712749a6329070ca
dongjun <rkawksha@naver.com>
2026-01-05 22:07:06 +0900

    로그인 구현 방식 변경중 - JWT Util 클래스 수정

--- backend/src/main/java/com/server/backend/common/auth/jwt/filter/CustomLogoutFilter.java
+++ backend/src/main/java/com/server/backend/common/auth/jwt/filter/CustomLogoutFilter.java
@@ -30,72 +30,72 @@ public class CustomLogoutFilter extends GenericFilterBean {
         doFilter((HttpServletRequest) request, (HttpServletResponse) response, chain);
     }
 
-    private void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
-        log.info("## CustomLogoutFilter doFilter");
-
-        // path and method verify
-        String requestUri = request.getRequestURI();
-        if(!requestUri.matches("^\\/logout$")) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-
-        String requestMethod = request.getMethod();
-        if(!requestMethod.equals("POST")) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-
-        // get refresh token
-        String refresh = null;
-        Cookie[] cookies = request.getCookies();
-        for(Cookie cookie: cookies) {
-            if(cookie.getName().equals("refresh")) {
-                refresh = cookie.getValue();
-            }
-        }
-
-        // refresh null check
-        if(refresh == null) {
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            return;
-        }
-
-        // expired check
-        try{
-            jwtUtil.isExpired(refresh);
-        } catch(ExpiredJwtException e) {
-            // response status code
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            return;
-        }
-
-        // 토큰이 refresh인지 확인 (발급시 페이로드에 명시)
-        String category = jwtUtil.getCategory(refresh);
-        if(!category.equals("refresh")) {
-            // response status code
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            return;
-        }
-
-        // DB에 저장되어 있는지 확인
-        Boolean isExist = userTokenRepository.existsByRefreshToken(refresh);
-        if(!isExist) {
-            // response status code
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            return;
-        }
-
-        // 로그아웃 진행
-        // refresh token DB에서 제거
-        userTokenRepository.deleteByRefreshToken(refresh);
-
-        // refresh token Cookie 값 0
-        Cookie cookie = new Cookie("refresh", null);
-        cookie.setMaxAge(0);
-        cookie.setPath("/");
-
-        response.addCookie(cookie);
-        response.setStatus(HttpServletResponse.SC_OK);
-    }
+//    private void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+//        log.info("## CustomLogoutFilter doFilter");
+//
+//        // path and method verify
+//        String requestUri = request.getRequestURI();
+//        if(!requestUri.matches("^\\/logout$")) {
+//            filterChain.doFilter(request, response);
+//            return;
+//        }
+//
+//        String requestMethod = request.getMethod();
+//        if(!requestMethod.equals("POST")) {
+//            filterChain.doFilter(request, response);
+//            return;
+//        }
+//
+//        // get refresh token
+//        String refresh = null;
+//        Cookie[] cookies = request.getCookies();
+//        for(Cookie cookie: cookies) {
+//            if(cookie.getName().equals("refresh")) {
+//                refresh = cookie.getValue();
+//            }
+//        }
+//
+//        // refresh null check
+//        if(refresh == null) {
+//            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+//            return;
+//        }
+//
+//        // expired check
+//        try{
+//            jwtUtil.isExpired(refresh);
+//        } catch(ExpiredJwtException e) {
+//            // response status code
+//            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+//            return;
+//        }
+//
+//        // 토큰이 refresh인지 확인 (발급시 페이로드에 명시)
+//        String category = jwtUtil.getCategory(refresh);
+//        if(!category.equals("refresh")) {
+//            // response status code
+//            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+//            return;
+//        }
+//
+//        // DB에 저장되어 있는지 확인
+//        Boolean isExist = userTokenRepository.existsByRefreshToken(refresh);
+//        if(!isExist) {
+//            // response status code
+//            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+//            return;
+//        }
+//
+//        // 로그아웃 진행
+//        // refresh token DB에서 제거
+//        userTokenRepository.deleteByRefreshToken(refresh);
+//
+//        // refresh token Cookie 값 0
+//        Cookie cookie = new Cookie("refresh", null);
+//        cookie.setMaxAge(0);
+//        cookie.setPath("/");
+//
+//        response.addCookie(cookie);
+//        response.setStatus(HttpServletResponse.SC_OK);
+//    }
 }
--- backend/src/main/java/com/server/backend/common/auth/jwt/util/JWTUtil.java
+++ backend/src/main/java/com/server/backend/common/auth/jwt/util/JWTUtil.java
@@ -1,6 +1,9 @@
 package com.server.backend.common.auth.jwt.util;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -9,15 +12,25 @@ import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
+@Slf4j
 @Component
 public class JWTUtil {
 
     private final SecretKey secretKey;
+    private final Long accessExpirationMs;
+    private final Long refreshExpirationMs;
 
-    public JWTUtil(@Value("${jwt.secret}") String secret) {
+    public JWTUtil(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.access-expiration-ms}") Long accessExpirationMs,
+            @Value("${jwt.refresh-expiration-ms}") Long refreshExpirationMs
+    ) {
         this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+        this.accessExpirationMs = accessExpirationMs;
+        this.refreshExpirationMs = refreshExpirationMs;
     }
 
+    // JWT 클레임 username 파싱
     public String getUsername(String token) {
         return Jwts
                 .parser()
@@ -25,9 +38,10 @@ public class JWTUtil {
                 .build()
                 .parseSignedClaims(token) // 클래임 확인
                 .getPayload() // 페이로드 부분에서 특정 데이터 가져오기
-                .get("username", String.class); // 특정 데이터를 get으로 가져오며 형식은 String으로 지정
+                .get("sub", String.class); // 특정 데이터를 get으로 가져오며 형식은 String으로 지정
     }
 
+    // JWT 클레임 role 파싱
     public String getRole(String token) {
         return Jwts
                 .parser()
@@ -38,35 +52,62 @@ public class JWTUtil {
                 .get("role", String.class);
     }
 
-    public String getCategory(String token) {
-        return Jwts
-                .parser()
-                .verifyWith(secretKey)
-                .build()
-                .parseSignedClaims(token)
-                .getPayload()
-                .get("category", String.class);
+//    public String getCategory(String token) {
+//        return Jwts
+//                .parser()
+//                .verifyWith(secretKey)
+//                .build()
+//                .parseSignedClaims(token)
+//                .getPayload()
+//                .get("category", String.class);
+//    }
+//
+//    public Boolean isExpired(String token) {
+//        return Jwts
+//                .parser()
+//                .verifyWith(secretKey)
+//                .build()
+//                .parseSignedClaims(token)
+//                .getPayload()
+//                .getExpiration()
+//                .before(new Date());
+//    }
+
+    // JWT 유효 여부(위조, 시간, Access/Refresh 여부)
+    public Boolean isValid(String token, Boolean isAccess) {
+        try{
+            Claims claims = Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+
+            String type = claims.get("type", String.class);
+            if(type == null) return false;
+
+            if(isAccess && !type.equals("access")) return false;
+            if(!isAccess && !type.equals("refresh")) return false;
+
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
     }
 
-    public Boolean isExpired(String token) {
-        return Jwts
-                .parser()
-                .verifyWith(secretKey)
-                .build()
-                .parseSignedClaims(token)
-                .getPayload()
-                .getExpiration()
-                .before(new Date());
-    }
+    // JWT(Access/Refresh) 생성
+    public String createJwt(String username, String role, Boolean isAccess) {
 
-    public String createJwt(String category, String username, String role, Long expiredMs) {
+        long now = System.currentTimeMillis();
+        long expiry = isAccess ? accessExpirationMs : refreshExpirationMs;
+        String type = isAccess ? "access" : "refresh";
+
         return Jwts
                 .builder()
-                .claim("category", category)
-                .claim("username", username) // payload 부분에 넣을 데이터
+                .claim("sub", username) // payload 부분에 넣을 데이터
                 .claim("role", role) // payload 부분에 넣을 데이터
-                .issuedAt(new Date(System.currentTimeMillis())) // 현재 발행 시간
-                .expiration(new Date(System.currentTimeMillis() + expiredMs)) // 이 토큰이 언제 소멸될지 설정
+                .claim("type", type) // payload 부분에 넣을 데이터
+                .issuedAt(new Date(now)) // 현재 발행 시간
+                .expiration(new Date(now + expiry))// 이 토큰이 언제 소멸될지 설정 (생명주기)
                 .signWith(secretKey) // 시크릿키를 통해 시그니처를 만들어 암호화 진행
                 .compact(); // 토큰을 컴팩트 하여 리턴, 위에서 설정한 Header + Payload + Signature를 .으로 구분된 문자열로 압축
     }

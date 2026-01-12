cd657c8c886a28eb9babc522c1d75b0febd75ae8
dongjun <rkawksha@naver.com>
2026-01-13 01:41:08 +0900

    react router 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import SignupPage from "./views/user/signup/Signup";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <>Commit Test</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ frontend/src/views/user/signup/Signup.jsx
@@ -0,0 +1,12 @@
+// .env로 부터 받은 백엔드 URL 받아오기
+const BACKEND_API_BASE_URL = import.meta.env.VITE_BACKEND_API_BASE_URL;
+
+function SignupPage() {
+  return (
+    <div>
+      <h1>회원가입 페이지</h1>
+    </div>
+  );
+}
+
+export default SignupPage;

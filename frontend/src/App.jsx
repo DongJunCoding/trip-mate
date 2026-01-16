7b3a253d5957b89f31ce152fef6519c78ff74147
dongjun <rkawksha@naver.com>
2026-01-17 01:55:36 +0900

    로그인 테스트 및 axios instance, interceptor 설정중

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import SignupPage from "./views/user/signup/Signup";
+import LoginPage from "./views/user/login/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/aaa" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/views/user/login/Login.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import axios from "axios";
+
+function LoginPage() {
+  const [userId, setUserId] = useState("");
+  const [userPw, setUserPw] = useState("");
+
+  const loginApi = async () => {
+    try {
+      const res = await axios.post("/login", {
+        userId: userId,
+        userPw: userPw,
+      });
+
+      console.log("res: ", res);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <h1>Login</h1>
+        <label>아이디</label> &nbsp;
+        <input
+          type="text"
+          placeholder="아이디"
+          value={userId}
+          onChange={(e) => setUserId(e.target.value)}
+        />
+        &nbsp;
+        <label>비밀번호</label> &nbsp;
+        <input
+          type="password"
+          placeholder="비밀번호"
+          value={userPw}
+          onChange={(e) => setUserPw(e.target.value)}
+        />
+      </div>
+      <button onClick={loginApi}>로그인</button>
+    </div>
+  );
+}
+
+export default LoginPage;
